9f82be587382e2b02b4c307527226ed4b0bb8d88
akashkumarsky <akash220701@gmail.com>
2025-08-19 12:36:31 +0530

    change db password and add package path for jpareop

--- backend/src/main/java/com/sky/Nykaa/NykaaApplication.java
+++ backend/src/main/java/com/sky/Nykaa/NykaaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.sky.Nykaa")
 public class NykaaApplication {
 
 	public static void main(String[] args) {
